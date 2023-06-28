e4d31ea701e45b81b913db12dd77b7cba00bd8ff
swathi <uttaralaswathi1998@gmail.com>
2023-06-28 11:02:15 +0000

    todo

--- app.js
+++ app.js
@@ -28,16 +28,16 @@ const initializeDBAndServer = async () => {
 initializeDBAndServer();
 const hasPriorityAndStatusProperties = (requestQuery) => {
   return (
-    requestQuery.status === undefined && requestQuery.priority === undefined
+    requestQuery.status !== undefined && requestQuery.priority !== undefined
   );
 };
 
 const hasStatusProperty = (requestQuery) => {
-  return requestQuery.status === undefined;
+  return requestQuery.status !== undefined;
 };
 
 const hasPriorityProperty = (requestQuery) => {
-  return requestQuery.priority === undefined;
+  return requestQuery.priority !== undefined;
 };
 
 app.get("/todos/", async (request, response) => {
@@ -50,15 +50,19 @@ app.get("/todos/", async (request, response) => {
              todo like '%${search_q}%' and status='${status}' and priority='${priority}';`;
       break;
     case hasPriorityProperty(request.query):
-      getTodosQuery = `select *from todo  where 
+      getTodosQuery = `select * from todo  where 
             todo like '%${search_q}%'  and priority='${priority}';`;
       break;
     case hasStatusProperty(request.query):
       getTodosQuery = `select *from todo  where 
             todo like '%${search_q}%' and status='${status}'  ;`;
       break;
+    default:
+      getTodosQuery = `select *from todo  where 
+            todo like '%${search_q}%';`;
   }
   data = await db.all(getTodosQuery);
+  response.send(data);
 });
 
 app.get("/todos/:todoId/", async (request, response) => {
@@ -81,13 +85,13 @@ app.put("/todos/:todoId/", async (request, response) => {
   let updateColumn = "";
   const requestBody = request.body;
   switch (true) {
-    case requestBody.status === undefined:
+    case requestBody.status !== undefined:
       updateColumn = "Status";
       break;
-    case requestBody.priority === undefined:
+    case requestBody.priority !== undefined:
       updateColumn = "Priority";
       break;
-    case requestBody.todo === undefined:
+    case requestBody.todo !== undefined:
       updateColumn = "Todo";
       break;
   }
